a98330eb783778dd5c5fdf21931e1cd047f60862
yeonjae <yeoncom11@gmail.com>
2025-10-27 20:06:07 +0900

    feat(Validator) : 입력 이름 길이 검증 로직 추가
    - 이름이 비어있는 경우, 길이가 5 초과인 경우 Exception 발생

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -3,10 +3,22 @@ package racingcar;
 public class Validator {
     public String[] validateCarName(String userInput) {
         try{
-            return userInput.split(",");
+            String[] nameList = userInput.split(",");
+            if(!checkNameLength(nameList)){
+                throw new IllegalArgumentException();
+            }
+            return nameList;
         }catch(Exception e) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public boolean checkNameLength(String[] nameList) {
+        for (String name : nameList) {
+            if(name.length()>5) return false;
+            if(name.isEmpty()) return false;
+        }
+        return true;
     }
 
     public int validateRoundCount(String userInput) {
